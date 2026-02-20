3500c3108b525cc5743ae771c360140fc0bd6c87
Gomathi122004 <gomathigomathi1030@gmail.com>
2026-02-20 20:29:47 +0530

    blog

--- src/component/blog.jsx
+++ src/component/blog.jsx
@@ -1,31 +1,4 @@
-import axios from "axios";
-import { useEffect } from "react";
 
-export const Blog = ()=>{
-    async function getpost(){
-        try{
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await res.json();
-            console.log (data);
-            }catch(err){
-                console.log(err);
-            }
-    }
-     async function getpostusingAxios(){
-        try{
-            await axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
-                console.log(res.data)
-    })
-}catch(err){
-                console.log("error fetching API",err);
-            }
-        }
-    // useEffect(() => {
-    //     getpost();
-    // }, [])
-useEffect(() => {
-        getpostusingAxios();
-    }, [])
 
 return(
     <div>
